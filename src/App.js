92c76de90dda1e549e4ac951dcfac21f1ac8b187
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-09-01 00:43:10 -0700

    update site name

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import About from './components/About';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Services from './components/Services';
-import Tours from './components/Tours';
+// import Services from './components/Services';
+// import Tours from './components/Tours';
 import ResumePDF from './components/ResumePDF'; // Import the ResumePDF component
 import Projects from './components/Projects';
 
