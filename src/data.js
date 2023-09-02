501f1caab18eb551f7e202524ec5f0c76122e06e
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-09-02 14:14:54 -0700

    update projects

--- src/data.js
+++ src/data.js
@@ -96,67 +96,165 @@ export const tours = [
 
 export const projects = [
   {
-    id: 1,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow.Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms`, `Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%`, `Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection zaccuracy by 25% with less false alerts and lowering the cost of inspecting suspicious data.`],
-    keywords: ['html', 'javascript', 'css', 'html', 'javascript', 'css', 'html', 'javascript', 'css'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 1,
+    "image": "guangyao-react-portfolio-2309",
+    "title": "React Responsive Portfolio Website",
+    "info": [
+      "Created a responsive portfolio website using React."
+    ],
+    "keywords": ["React", "Portfolio", "Responsive Design"],
+    "link": "https://github.com/Dave-Guangyao-Li/guangyao-react-portfolio-2309"
   },
   {
-    id: 2,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow`],
-    keywords: ['html', 'javascript'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 2,
+    "image": "medical-coding-copilot",
+    "title": "MediCode AI Copilot Chatbot",
+    "info": [
+      "Developed an AI copilot chatbot for medical coding."
+    ],
+    "keywords": ["Chatbot", "AI", "Medical Coding"],
+    "link": "https://github.com/Dave-Guangyao-Li/medical-coding-copilot"
   },
   {
-    id: 3,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow.Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms`, `Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%`, `Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection zaccuracy by 25% with less false alerts and lowering the cost of inspecting suspicious data.`],
-    keywords: ['html', 'javascript', 'css', 'html', 'javascript', 'css', 'html', 'javascript', 'css'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 3,
+    "image": "Sentient",
+    "title": "Sentient Financial Platform",
+    "info": [
+      "Built a financial platform called Sentient."
+    ],
+    "keywords": ["Financial", "Platform"],
+    "link": "https://github.com/Dave-Guangyao-Li/Sentient"
   },
   {
-    id: 4,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow.Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms`, `Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%`, `Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection zaccuracy by 25% with less false alerts and lowering the cost of inspecting suspicious data.`],
-    keywords: ['html', 'javascript', 'css', 'html', 'javascript', 'css', 'html', 'javascript', 'css'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 4,
+    "image": "Hexagon-App",
+    "title": "Hexagon Movies and TV Series Info",
+    "info": [
+      "Created Hexagon, an app for movies and TV series information."
+    ],
+    "keywords": ["Movies", "TV Series", "Information"],
+    "link": "https://github.com/Dave-Guangyao-Li/Hexagon-App"
   },
   {
-    id: 5,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow.Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms`, `Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%`, `Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection zaccuracy by 25% with less false alerts and lowering the cost of inspecting suspicious data.`],
-    keywords: ['html', 'javascript', 'css', 'html', 'javascript', 'css', 'html', 'javascript', 'css'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 5,
+    "image": "React-task-manager-Dave-Guangyao-Li",
+    "title": "React Task Manager App",
+    "info": [
+      "Developed a task manager app using React."
+    ],
+    "keywords": ["React", "Task Manager"],
+    "link": "https://github.com/Dave-Guangyao-Li/React-task-manager-Dave-Guangyao-Li"
   },
   {
-    id: 6,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow.Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms`, `Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%`, `Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection zaccuracy by 25% with less false alerts and lowering the cost of inspecting suspicious data.`],
-    keywords: ['html', 'javascript', 'css', 'html', 'javascript', 'css', 'html', 'javascript', 'css'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 6,
+    "image": "movie-pitcher-openai",
+    "title": "Movie Pitch Generator with ChatGPT and DALL-E",
+    "info": [
+      "Designed a movie pitch generator using ChatGPT and DALL-E."
+    ],
+    "keywords": ["Movie Pitch", "ChatGPT", "DALL-E"],
+    "link": "https://github.com/Dave-Guangyao-Li/movie-pitcher-openai"
   },
   {
-    id: 7,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow.Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms`, `Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%`, `Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection zaccuracy by 25% with less false alerts and lowering the cost of inspecting suspicious data.`],
-    keywords: ['html', 'javascript', 'css', 'html', 'javascript', 'css', 'html', 'javascript', 'css'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 7,
+    "image": "simple-dalle-clone",
+    "title": "DALL-E Clone Website",
+    "info": [
+      "Created a website that clones DALL-E."
+    ],
+    "keywords": ["DALL-E", "Website Clone"],
+    "link": "https://github.com/Dave-Guangyao-Li/simple-dalle-clone"
   },
   {
-    id: 8,
-    image: MSCIProject,
-    title: 'Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology',
-    info: [`Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow.Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms`, `Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%`, `Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection zaccuracy by 25% with less false alerts and lowering the cost of inspecting suspicious data.`],
-    keywords: ['html', 'javascript', 'css', 'html', 'javascript', 'css', 'html', 'javascript', 'css'],
-    link: 'https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing'
+    "id": 8,
+    "image": "fine-tuned-GPT-chatbot",
+    "title": "Fine-Tuned GPT Chatbot",
+    "info": [
+      "Developed a fine-tuned GPT chatbot."
+    ],
+    "keywords": ["GPT", "Chatbot"],
+    "link": "https://github.com/Dave-Guangyao-Li/fine-tuned-GPT-chatbot"
+  },
+  {
+    "id": 9,
+    "image": "expense-tracker-nextjs-firebase-tailwind",
+    "title": "NextJS Expense Tracker",
+    "info": [
+      "Built an expense tracker using NextJS and Firebase Firestore, styled with TailwindCSS."
+    ],
+    "keywords": ["NextJS", "Expense Tracker", "Firebase", "TailwindCSS"],
+    "link": "https://github.com/Dave-Guangyao-Li/expense-tracker-nextjs-firebase-tailwind"
+  },
+  {
+    "id": 10,
+    "image": "car-sales-site-scrimba",
+    "title": "Cars Sales Website",
+    "info": [
+      "Developed a car sales website."
+    ],
+    "keywords": ["Car Sales", "Website"],
+    "link": "https://github.com/Dave-Guangyao-Li/car-sales-site-scrimba"
+  },
+  {
+    "id": 11,
+    "image": "data-analytics-dashboard",
+    "title": "Analytics Dashboard",
+    "info": [
+      "Created an analytics dashboard."
+    ],
+    "keywords": ["Analytics", "Dashboard"],
+    "link": "https://github.com/Dave-Guangyao-Li/data-analytics-dashboard"
+  },
+  {
+    "id": 12,
+    "image": "react-weather-app",
+    "title": "React Weather Forecast",
+    "info": [
+      "Developed a weather forecast app using React."
+    ],
+    "keywords": ["React", "Weather Forecast"],
+    "link": "https://github.com/Dave-Guangyao-Li/react-weather-app"
+  },
+  {
+    "id": 13,
+    "image": "GithubProfileCard",
+    "title": "GitHub Profile Card",
+    "info": [
+      "Created a GitHub profile card."
+    ],
+    "keywords": ["GitHub", "Profile Card"],
+    "link": "https://github.com/Dave-Guangyao-Li/GithubProfileCard"
+  },
+  {
+    "id": 14,
+    "image": MSCIProject,
+    "title": "Enhancing MSCI’s Anomaly Detection Workflow by Integrating Machine Learning into Existing Financial Technology",
+    "info": [
+      "Anomaly Detection Workflow: Overhauled existing Anomalous Data Detection workflow. Improved overall precision by codebase profiling, incorporating new Data Cleaning methods and Machine Learning algorithms",
+      "Customizable ETL Module: Implemented a modularized and cacheable Data Preparation and Loading process module with Python, reducing execution time by 35% and increasing execution stability by 20%",
+      "Algorithm Evaluation Pipeline: Developed a solid Algorithms Analysis and Evaluation Pipeline, increasing detection accuracy by 25% with fewer false alerts and lowering the cost of inspecting suspicious data."
+    ],
+    "keywords": ["Data Analytics", "Python", "Data Engineering", "Machine Learning", "Statistics", "Javascript", "Numpy", "Pandas"],
+    "link": "https://drive.google.com/file/d/1aetZ8dsqWVH2tCFoEgzm6eGKeuVxPJ7m/view?usp=sharing"
+  },
+  {
+    "id": 15,
+    "image": "The-Lagging-Effect-of-COVID-19-vaccines",
+    "title": "The Lagging Effect of COVID-19 Vaccines on the Case-Fatality Rate in California",
+    "info": [
+      "Analyze the lagging effect of COVID-19 vaccines on the case-fatality rate in California."
+    ],
+    "keywords": ["COVID-19", "Vaccines", "California", "Analysis"],
+    "link": "https://drive.google.com/file/d/10J5_n_VqubL8XDpM2HQ-ljo2LlrsDJdp/view?usp=sharing"
+  },
+  {
+    "id": 16,
+    "image": "Game-Prediction-for-Golden-State-Warriors",
+    "title": "Game Prediction for the Golden State Warriors in the NBA Regular Season",
+    "info": [
+      "Predicted game outcomes for the Golden State Warriors in the NBA regular season."
+    ],
+    "keywords": ["NBA", "Golden State Warriors", "Game Prediction"],
+    "link": "https://drive.google.com/file/d/1EGGuWsQZdI6PBZwJL-1pTGq43q-kf37K/view?usp=sharing"
   }
-];
+]
