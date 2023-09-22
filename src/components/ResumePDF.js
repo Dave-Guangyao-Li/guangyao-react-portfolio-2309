3d13bd5b94d67bbbbd7be557d0c70d0b111ff8d7
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-09-22 14:31:22 -0700

    new resume

--- src/components/ResumePDF.js
+++ src/components/ResumePDF.js
@@ -7,7 +7,7 @@ const ResumePDF = () => {
             <div className='pdf-container'>
                 <iframe
                     title='Resume PDF'
-                    src='https://drive.google.com/file/d/1tWH4b77yLL48gqXvGzMVPspB-gCp7Br8/preview'
+                    src='https://drive.google.com/file/d/1Iscf0Yk4E-TZauuvmzgKLJWBpCupg7Wn/preview'
                     width='100%'
                     height='600'
                     frameborder='0'
